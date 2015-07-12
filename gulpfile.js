85d08bc1dedd25d4078a4b9fbcfef95ed90c4d48
Oleh Zasadnyy <oleh.zasadnyy@gmail.com>
2015-07-12 20:36:32 +0300

    Added 2014 site

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,3 @@
-/*
-Copyright (c) 2015 The Polymer Project Authors. All rights reserved.
-This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
-The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
-The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
-Code distributed by Google as part of the polymer project is also
-subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
-*/
-
 'use strict';
 
 // Include Gulp & Tools We'll Use
@@ -186,7 +177,7 @@ gulp.task('precache', function (callback) {
 gulp.task('gh-pages', function() {
   return gulp.src('./dist/**/*')
     .pipe(ghPages({
-      remoteUrl: 'https://github.com/ozasadnyy/devfest-ukraine-15'
+      remoteUrl: 'https://github.com/GDG-Ukraine/devfest.gdg.org.ua'
     }));
 });
 
